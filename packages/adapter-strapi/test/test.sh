9ca9c4164969de52a87f9d4bea860cca41bd72e4
henrik-clabs <henrik.sorensen@cinfinitysolutions.com>
2024-10-18 00:01:38 +0000

    adapter-strapi: test.sh

---
+++ packages/adapter-strapi/test/test.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+CONTAINER_NAME=authjs-strapi-test
+IMAGE_NAME=authjs-strapi-image
+CONTAINER_PORT="1337:1337"
+
+
+cat << EOF >test/dockerfile
+FROM node:18-alpine3.18
+# Installing libvips-dev for sharp Compatibility
+RUN apk update && apk add --no-cache build-base gcc autoconf automake zlib-dev libpng-dev nasm bash vips-dev git nano
+ARG NODE_ENV=development
+ENV NODE_ENV=\${NODE_ENV}
+WORKDIR /opt/
+RUN yarn global add node-gyp
+RUN yarn config set network-timeout 600000 -g && yarn install
+ENV PATH=/opt/node_modules/.bin:\$PATH
+WORKDIR /opt/app
+RUN chown -R node:node /opt/app
+USER node
+ENV DATABASE_CLIENT=sqlite
+ENV DATABASE_FILENAME=testauthdata
+RUN ["yarn","create","strapi","testauth","--no-git-init","--skip-cloud","--js","--no-run","--dbclient","sqlite","--example","--install"]
+WORKDIR /opt/app/testauth
+RUN ["yarn", "build"]
+RUN ["yarn","strapi","admin:create","--email","strapi@noemail.com","--password","@uthStrap1","--firstname","strapi","--lastname","strapi"]
+RUN ["yarn","strapi","telemetry:disable"]
+COPY ./test/strapi_console_input.txt strapi_console_input.txt
+RUN echo ".load strapi_console_input.txt" | yarn strapi console
+RUN cat api_key.txt
+CMD ["yarn", "develop"]
+EOF
+
+
+cat << EOF >test/strapi_console_input.txt
+const fs = require('node:fs');
+const attributes = { name: 'authjstest', description: 'Token for Auth.js strapi adapter testing', type: 'full-access',lifespan: null, };
+const apiToken = await strapi.service('admin::api-token').create(attributes);
+const out = "API_KEY=" + apiToken.accessKey;
+await fs.writeFileSync('api_key.txt', out);
+EOF
+
+# Build image
+
+docker build -f test/dockerfile -t $IMAGE_NAME .
+RC=$?
+
+if [ $RC -ne 0 ]; then
+  echo "Build failed ..."
+  exit 1
+fi
+
+# extract api key for tests
+key=`docker run --rm \
+  -p ${CONTAINER_PORT} \
+  ${IMAGE_NAME} \
+  cat api_key.txt | grep API_ `
+
+echo "Found key: $key..."
+
+docker run -d --rm \
+  --name ${CONTAINER_NAME} \
+  -p ${CONTAINER_PORT} \
+  ${IMAGE_NAME}
+
+
+echo "waiting 5s for db to start..."
+sleep 5
+
+# Always stop container, but exit with 1 when tests are failing
+if vitest run -c ../utils/vitest.config.ts; then
+  echo "Tests succeeded... Stopping container..."
+  docker stop ${CONTAINER_NAME}
+else
+  echo "Tests failed... Stopping container..."
+  docker stop ${CONTAINER_NAME} && exit 1
+fi
